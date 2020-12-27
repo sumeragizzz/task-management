e7f73b56287fb1d741b445e3a27c1ab0a733f9a5
sumeragizzz <developer@sumeragizzz.dev>
2020-12-27 22:45:16 +0900

    テストクラスのパッケージ変更

--- src/test/java/dev/sumeragizzz/taskmanagement/domain/valueobject/TaskOverviewTest.java
+++ src/test/java/dev/sumeragizzz/taskmanagement/domain/valueobject/TaskOverviewTest.java
@@ -1,4 +1,4 @@
-package dev.sumeragizzz.taskmanagement.valueobject;
+package dev.sumeragizzz.taskmanagement.domain.valueobject;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
